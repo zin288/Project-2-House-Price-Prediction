3e54c390030e8357625a097a388fe434c88e2583
zin288 <jinjin28829@gmail.com>
2023-07-21 11:23:50 +0800

    Update Budgeting_Insights.py

--- app/pages/Budgeting_Insights.py
+++ app/pages/Budgeting_Insights.py
@@ -21,32 +21,32 @@ st.markdown("***Make your real estate plans with technology of the future***")
 # Using .cache_data so to reduce lag
 @st.cache_data
 def get_data(filename):
-    df = pd.read_csv(filename)
+	df = pd.read_csv(filename)
 
-    # Data needed for model 1
-    df_filtered = df[[  # Categorical data:
-                        'town', 'storey_range', 'full_flat_type', 'pri_sch_name',  
-                        # Numerical data:
-                        'floor_area_sqm', 'lease_commence_date', 'mall_nearest_distance', 'hawker_nearest_distance', 'mrt_nearest_distance', 
-                        'pri_sch_nearest_distance', 'sec_sch_nearest_dist', 'resale_price']]
-    # Model's Numerical data only
-    df_filtered_num = df[[  'floor_area_sqm', 'lease_commence_date', 'mrt_nearest_distance', 'hawker_nearest_distance',
-                            'mall_nearest_distance', 'pri_sch_nearest_distance', 'sec_sch_nearest_dist', 'resale_price']]
-    # Model's Categorical data only
-    df_filtered_cat = df[['town', 'storey_range', 'full_flat_type', 'pri_sch_name']]
+	# Data needed for model 1
+	df_filtered = df[[  # Categorical data:
+						'town', 'storey_range', 'full_flat_type', 'pri_sch_name',  
+						# Numerical data:
+						'floor_area_sqm', 'lease_commence_date', 'mall_nearest_distance', 'hawker_nearest_distance', 'mrt_nearest_distance', 
+						'pri_sch_nearest_distance', 'sec_sch_nearest_dist', 'resale_price']]
+	# Model's Numerical data only
+	df_filtered_num = df[[  'floor_area_sqm', 'lease_commence_date', 'mrt_nearest_distance', 'hawker_nearest_distance',
+							'mall_nearest_distance', 'pri_sch_nearest_distance', 'sec_sch_nearest_dist', 'resale_price']]
+	# Model's Categorical data only
+	df_filtered_cat = df[['town', 'storey_range', 'full_flat_type', 'pri_sch_name']]
 
 
-    # user_fr_dict will store the caterogrical values as a user-friendly form,
-    # by removing '_' and capitalising first letter of each word
-    user_fr_dict = {}
+	# user_fr_dict will store the caterogrical values as a user-friendly form,
+	# by removing '_' and capitalising first letter of each word
+	user_fr_dict = {}
 
-    # Iterate over each column in df_filtered_cat, get the unique values, and add to dictionary
-    for col in df_filtered_cat.columns:
-        unique_values = df_filtered_cat[col].unique()
-        transformed_unique_values = [value.replace('_', ' ').title() for value in unique_values]
-        user_fr_dict[col] = transformed_unique_values
+	# Iterate over each column in df_filtered_cat, get the unique values, and add to dictionary
+	for col in df_filtered_cat.columns:
+		unique_values = df_filtered_cat[col].unique()
+		transformed_unique_values = [value.replace('_', ' ').title() for value in unique_values]
+		user_fr_dict[col] = transformed_unique_values
 
-    return df, df_filtered, df_filtered_num, df_filtered_cat, user_fr_dict
+	return df, df_filtered, df_filtered_num, df_filtered_cat, user_fr_dict
 
 df, df_filtered, df_filtered_num, df_filtered_cat, user_fr_dict = get_data(Path(__file__).parent /'../housing_df.csv')
 
@@ -70,7 +70,7 @@ def show_eda():
 
 	# Allow the user to select columns and values
 	selected_column = st.selectbox("Select an attribute", df_filtered.columns)
-    st.write("Selected columns:",selected_column)
+	st.write("Selected columns:",selected_column)
 
 	# Filter the DataFrame based on the user's selection
 	# filtered_user_df = df_filtered[selected_column]
@@ -80,16 +80,16 @@ def show_eda():
 	if selected_column not in df_filtered_cat:
 		# Create a new column indicating whether each data point falls within the y-value range
 		df_filtered['color'] = np.where((df_filtered['resale_price'] >= y_range[0]) &
-		                                     (df_filtered['resale_price'] <= y_range[1]),
-		                                     'maroon', 'blue')
+												(df_filtered['resale_price'] <= y_range[1]),
+												'maroon', 'blue')
 		fig = px.scatter(df_filtered, x=selected_column, y="resale_price", color='color')
 
 	else:
 		# Calculate average resale prices by col
 		average_prices = filtered_user_df.groupby(selected_column)["resale_price"].mean().sort_values().reset_index()
 		filtered_prices = average_prices[
-	    (average_prices['resale_price'] >= budget_min) &
-	    (average_prices['resale_price'] <= budget_max)
+		(average_prices['resale_price'] >= budget_min) &
+		(average_prices['resale_price'] <= budget_max)
 	]
 
 		fig = px.bar(filtered_prices, x=selected_column, y="resale_price", color=selected_column)
